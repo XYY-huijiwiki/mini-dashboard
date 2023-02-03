2350e80d7a06319b122bb455c50322fdd74b79ef
Karsten Zhou <1339688323@qq.com>
2023-02-03 23:53:20 +0800

    文件列表添加图标

--- src/components/all-file.vue
+++ src/components/all-file.vue
@@ -3,13 +3,20 @@
     <n-space vertical>
         <n-list hoverable>
             <n-list-item v-for="item in fileList">
-                {{ item.title }}
+                <template #prefix>
+                    <n-icon :size="24">
+                        <music-icon v-if="musikExtList.includes((item.title.split('.').reverse())[0])" />
+                        <video-icon v-else-if="videoExtList.includes((item.title.split('.').reverse())[0])" />
+                        <file-icon v-else />
+                    </n-icon>
+                </template>
+                {{ item.title.replace('文件:', '') }}
                 <template #suffix>
                     <n-space :wrap="false">
                         <n-button tertiary tag="a" :href="`https://xyy.huijiwiki.com/p/${item.pageid}`"
                             target="blank">文件页面</n-button>
-                        <n-button tertiary type="error" disabled> <template #icon> <n-icon> <delete-icon /> </n-icon>
-                            </template>
+                        <n-button tertiary type="error" disabled>
+                            <template #icon><n-icon><trash-icon /></n-icon></template>
                         </n-button>
                     </n-space>
                 </template>
@@ -23,17 +30,80 @@
 
 <script setup>
 import { ref } from 'vue';
-// import sleep from 'await-sleep';
-import { TrashOutline as deleteIcon } from '@vicons/ionicons5';
+import sleep from 'await-sleep';
+import {
+    Music as musicIcon,
+    Video as videoIcon,
+    File as fileIcon,
+    TrashX as trashIcon,
+} from '@vicons/tabler';
 
 var showBtn = ref(true);
 var fileList = ref([]);
 var loading = ref(false);
 var cmcontinue = ref('');
+var musikExtList = ['mp3', 'mid'];
+var videoExtList = ['mp4'];
 
 async function showAllFile() {
 
     loading.value = true;
+
+    // 测试
+    await sleep(1000);
+    fileList.value = [
+        {
+            "pageid": 47828,
+            "ns": 6,
+            "title": "文件:01-Level-01.mp3"
+        },
+        {
+            "pageid": 47830,
+            "ns": 6,
+            "title": "文件:02-Level-02.mid"
+        },
+        {
+            "pageid": 47832,
+            "ns": 6,
+            "title": "文件:04-Level-04"
+        },
+        {
+            "pageid": 47834,
+            "ns": 6,
+            "title": "文件:Bonus levelsong-01.mp4"
+        },
+        {
+            "pageid": 47805,
+            "ns": 6,
+            "title": "文件:国潮有喜 button.mp3"
+        },
+        {
+            "pageid": 47811,
+            "ns": 6,
+            "title": "文件:国潮有喜 foot.mp3"
+        },
+        {
+            "pageid": 47809,
+            "ns": 6,
+            "title": "文件:国潮有喜 game.mp3"
+        },
+        {
+            "pageid": 47801,
+            "ns": 6,
+            "title": "文件:国潮有喜 gs.mp3"
+        },
+        {
+            "pageid": 47807,
+            "ns": 6,
+            "title": "文件:国潮有喜 select.mp3"
+        },
+        {
+            "pageid": 47803,
+            "ns": 6,
+            "title": "文件:国潮有喜 success.mp3"
+        }
+    ];
+    return;
 
     let response = await axios.get(encodeURI(`https://xyy.huijiwiki.com/api.php?action=query&list=categorymembers&cmtitle=分类:Base64编码的文件&format=json&cmcontinue=${cmcontinue.value}`));
     fileList.value = fileList.value.concat(response['data']['query']['categorymembers']);

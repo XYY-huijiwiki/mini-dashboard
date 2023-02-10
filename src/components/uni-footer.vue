2ba4f765805dcee4d1a8ecf6032af7b6ba3d0b49
Karsten Zhou <1339688323@qq.com>
2023-02-11 00:29:00 +0800

    删除功能暂时下架

--- src/components/uni-footer.vue
+++ src/components/uni-footer.vue
@@ -3,7 +3,7 @@
         <!-- 本地测试（开始） -->
         <n-p>当前为本地测试</n-p>
         <!-- 本地测试（结束） -->
-        <n-p>最后编译时间：Feb 11, 2023 12:24 AM</n-p>
+        <n-p>最后编译时间：Feb 11, 2023 12:28 AM</n-p>
         <n-space justify="end">
             <n-a v-if="pathname.includes(encodeURI('测试版'))"
                 :href="`//xyy.huijiwiki.com${pathname.replace(encodeURI('（测试版）'), '')}`">返回稳定版</n-a>

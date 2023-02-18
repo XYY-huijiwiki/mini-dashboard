49258901cf2b4cf0ecec406d3dd39c403855c191
Karsten Zhou <1339688323@qq.com>
2023-02-18 21:13:31 +0800

    更新build

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@
       <!-- 底部footer -->
       <template #action>
         <n-space justify="space-between">
-          <n-p>最后编译时间：2023年2月18日 20:53</n-p>
+          <n-p>最后编译时间：2023年2月18日 21:12</n-p>
           <n-a href="//github.com/XYY-huijiwiki" target="_blank">Github</n-a>
         </n-space>
       </template>

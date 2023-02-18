6916c824c1a0995a2c9e168ccbc3a3d61ec2b0a2
Karsten Zhou <1339688323@qq.com>
2023-02-18 20:53:35 +0800

    调整页脚

--- src/components/uni-footer.vue
+++ src/components/uni-footer.vue
@@ -1,16 +1,6 @@
 <template>
     <n-space justify="space-between">
-        <!-- 本地测试（开始） -->
-        <n-p>当前为本地测试</n-p>
-        <!-- 本地测试（结束） -->
-        <n-p>最后编译时间：2023年2月18日 20:47</n-p>
-        <n-space justify="end">
-            <n-a href="//github.com/XYY-huijiwiki" target="_blank">Github</n-a>
-        </n-space>
+        <n-p>最后编译时间：2023年2月18日 20:53</n-p>
+        <n-a href="//github.com/XYY-huijiwiki" target="_blank">Github</n-a>
     </n-space>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-let pathname = ref(location.pathname);
-</script>

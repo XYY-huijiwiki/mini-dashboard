ce792ea7a107d8e6d40d7785b351f46b56b08c29
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-11-16 13:24:15 +0800

    dev mode switcher update

---
+++ src/components/info-label.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-popover trigger="click">
+    <template #trigger>
+      <n-button text class="align-middle text-sm">
+        <n-icon :component="Info16Regular" class="info-label"></n-icon>
+      </n-button>
+    </template>
+    {{ content }}
+  </n-popover>
+</template>
+
+<script setup lang="ts">
+import { Info16Regular } from "@vicons/fluent";
+
+defineProps({
+  content: String,
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,33 @@ import { createPinia } from "pinia";
 import { userLang, langPacks } from "@/stores/locales";
 
 (async () => {
+  // dev mode (true for dev and false for prod)
+  const userDevModeOption: Boolean = JSON.parse(
+    localStorage.getItem("miniDashboardSettings") || `{"devMode":false}`
+  ).devMode;
+  const currentCodeDevMode: Boolean = import.meta.env.DEV;
+  if (userDevModeOption !== currentCodeDevMode) {
+    // if user dev mode is on but current code dev mode is off, show warning
+    if (userDevModeOption) {
+      const turnOffDevMode = function () {
+        localStorage.setItem(
+          "miniDashboardSettings",
+          JSON.stringify(
+            JSON.parse(localStorage.getItem("miniDashboardSettings") || "{}")
+          )
+        );
+        location.reload();
+      };
+      const devModeWarningHTML = `Waiting dev server... <a onclick="(${turnOffDevMode.toString()})()">Click Here</a> to turn off dev mode.`;
+      document.querySelector("#mini-dashboard")!.innerHTML = devModeWarningHTML;
+    } else {
+      console.warn(
+        "[Mini Dashboard]User dev mode is off. Turn on dev mode on settings to load dev server."
+      );
+    }
+    return;
+  }
+
   // i18n
   const language = JSON.parse(
     localStorage.getItem("miniDashboardSettings") || `{"language":"auto"}`
